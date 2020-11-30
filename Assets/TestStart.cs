dfd72c1ca151171a3880b6b977811cae417c9aca
mr_xin321 <mr_xin321@163.com>
2020-11-30 20:41:40 +0800

    提交工具

---
+++ Assets/TestStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var prefab = Resources.Load("hh/TestPrefab");
+        var go = GameObject.Instantiate(prefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
