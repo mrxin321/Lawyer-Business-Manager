bd8f1aaef78824521de490b6e2cc2f1e72df9c63
ziwei <ziwei.com>
2020-12-02 08:15:53 +0800

    111

---
+++ Assets/Scripts/UI/BaseView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void onClose()
+    {
+    	GameObject.Destroy(gameObject);
+    }
+}
