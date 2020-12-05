aa2370afa2b0ec9a21b56f8aa9a53cbbc002d504
mr_xin321 <mr_xin321@163.com>
2020-12-05 17:10:59 +0800

    1

--- Assets/Scripts/Tools/Sqlite/SqliteTest.cs
+++ Assets/Scripts/Tools/Sqlite/SqliteTest.cs
@@ -33,47 +33,47 @@ public class SqliteTest : MonoBehaviour
     {
         
 
-        btn_Insert.onClick.AddListener(delegate()
-        {
-            string addContent = DateTime.Now.ToString();
-            Hashtable hashtable = new Hashtable();
-            hashtable.Add(0,Random.Range(1,100));
-            hashtable.Add(1,addContent);
-            hashtable.Add(2,0);
-            _sqliteManager.InsertValue(tableName,new string[]{"testId","content","state"},hashtable);
-        });
+        // btn_Insert.onClick.AddListener(delegate()
+        // {
+        //     string addContent = DateTime.Now.ToString();
+        //     Hashtable hashtable = new Hashtable();
+        //     hashtable.Add(0,Random.Range(1,100));
+        //     hashtable.Add(1,addContent);
+        //     hashtable.Add(2,0);
+        //     _sqliteManager.InsertValue(tableName,new string[]{"testId","content","state"},hashtable);
+        // });
         
-        btn_Print.onClick.AddListener(delegate
-        {
-            SqliteDataReader dataReader= _sqliteManager.SelectAllParam(tableName);
+//         btn_Print.onClick.AddListener(delegate
+//         {
+//             SqliteDataReader dataReader= _sqliteManager.SelectAllParam(tableName);
             
-            string showContent = "";
-            while(dataReader != null && dataReader.Read()) 
-            {
-                //读取ID
-//                Debug.Log(dataReader.GetInt32(dataReader.GetOrdinal("testId")));
-//                //读取Name
-//                Debug.Log(dataReader.GetString(dataReader.GetOrdinal("content")));
-//                Debug.Log(dataReader.GetInt32(dataReader.GetOrdinal("state")));
-                int msgId = dataReader.GetInt32(dataReader.GetOrdinal("id"));
-                int testId = dataReader.GetInt32(dataReader.GetOrdinal("testId"));
+//             string showContent = "";
+//             while(dataReader != null && dataReader.Read()) 
+//             {
+//                 //读取ID
+// //                Debug.Log(dataReader.GetInt32(dataReader.GetOrdinal("testId")));
+// //                //读取Name
+// //                Debug.Log(dataReader.GetString(dataReader.GetOrdinal("content")));
+// //                Debug.Log(dataReader.GetInt32(dataReader.GetOrdinal("state")));
+//                 int msgId = dataReader.GetInt32(dataReader.GetOrdinal("id"));
+//                 int testId = dataReader.GetInt32(dataReader.GetOrdinal("testId"));
 
-                string content = dataReader.GetString(dataReader.GetOrdinal("content"));
+//                 string content = dataReader.GetString(dataReader.GetOrdinal("content"));
 
-                int state = dataReader.GetInt32(dataReader.GetOrdinal("state"));
-                showContent += string.Format("(id: {0} testId : {1},content : {2}, state : {3} )",msgId ,testId, content, state) + "\n";
-            }
+//                 int state = dataReader.GetInt32(dataReader.GetOrdinal("state"));
+//                 showContent += string.Format("(id: {0} testId : {1},content : {2}, state : {3} )",msgId ,testId, content, state) + "\n";
+//             }
 
-            contentText.text =string.IsNullOrEmpty(showContent)?"表中还没有数据":showContent;
-            if(dataReader!=null)
-                dataReader.Close();
-        });
+//             contentText.text =string.IsNullOrEmpty(showContent)?"表中还没有数据":showContent;
+//             if(dataReader!=null)
+//                 dataReader.Close();
+//         });
         
-        btn_Delete.onClick.AddListener(delegate
-        {
-            string sqlStr = "DELETE FROM test WHERE testId = 10";
-            _sqliteManager.ExecuteQuery(tableName,sqlStr);
-        });
+        // btn_Delete.onClick.AddListener(delegate
+        // {
+        //     string sqlStr = "DELETE FROM test WHERE testId = 10";
+        //     _sqliteManager.ExecuteQuery(tableName,sqlStr);
+        // });
         
     }
 
--- Assets/Scripts/UI/BaseView.cs
+++ Assets/Scripts/UI/BaseView.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class BaseView : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Canvas mainCanvas;
+    private static int Layer = 0;
+    void Start()
+    {
+    	if(mainCanvas == null)
+    	{
+    		mainCanvas = gameObject.GetComponent<Canvas>();
+    	}
+    	Layer += 1;
+    	mainCanvas.sortingOrder = Layer;
+    }
+
     public void onClose()
     {
     	GameObject.Destroy(gameObject);
