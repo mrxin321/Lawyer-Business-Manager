a6b43490b469fae889db4b768588e164510b1dea
DESKTOP-3ILGG7O\xin <mr_xin321@163.com>
2021-05-23 21:47:54 +0800

    修改

---
+++ Assets/Scripts/UI/CaseManager/CaseCheckView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaseCheckView : MonoBehaviour
+{
+	// public int Id;
+ //    public string Name;
+ //    public int Master;
+ //    public int CaseType;
+ //    public string ContractId;
+ //    public string Customer;
+ //    public string Content;
+ //    public string Mask;
+ //    public string Plaintiff;
+ //    public string Defendant;
+ //    public string Other;
+ //    public string Institution;
+ //    public int Paytype;
+ //    public string Money;
+ //    public string Paydes;
+ //    public string Createtime;
+    [SerializeField] Text CaseId;
+    [SerializeField] Text CaseName;
+    [SerializeField] Text ContractId;
+    [SerializeField] Text Customer;
+    [SerializeField] Text Mask;
+    [SerializeField] Text Plaintiff;
+}
---
+++ Assets/Scripts/UI/Common/BottomFuncManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomFuncManager: MonoBehaviour
+{
+	public void ToListClick()
+	{
+		UIManager.Instance.CloseAllWindow();
+    	UIManager.Instance.OpenWindow("ToDoListView");
+	}    
+
+	public void MyCaseClick()
+	{
+		UIManager.Instance.CloseAllWindow();
+    	UIManager.Instance.OpenWindow("MyCaseListView");
+	}    
+	public void CaseManagerClick()
+	{
+		UIManager.Instance.CloseAllWindow();
+    	UIManager.Instance.OpenWindow("CaseTotalView");
+	}    
+	public void UserClick()
+	{
+		UIManager.Instance.CloseAllWindow();
+    	UIManager.Instance.OpenWindow("UserView");
+	}    
+
+}
